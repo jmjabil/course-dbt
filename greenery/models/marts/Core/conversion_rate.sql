569ed3974f4e7e860715e236b29cd93634bd2f8c
jmjabil <joshua_milford@jabil.com>
2022-06-27 02:22:05 +0000

    week 2

---
+++ greenery/models/marts/Core/conversion_rate.sql
@@ -0,0 +1,21 @@
+with 
+events as (
+    SELECT * FROM {{ ref('stg_events') }}
+),
+checkouts as (
+    SELECT * FROM {{ ref('stg_events') }}
+    WHERE event_type = 'checkout'
+),
+int as (
+SELECT 
+COUNT(DISTINCT b.session_id) AS numer,
+COUNT(DISTINCT a.session_id) AS denom
+--AS conversion_rate
+FROM events a 
+JOIN checkouts b 
+on 1=1
+)
+
+SELECT 
+ROUND(numer*100.0/denom, 1) AS PERCENT
+FROM int
